151fc5b163e07309be51704d55c439b1477c57ba
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-08-01 19:44:19 +0530

    feat: Added Gender property to Person class

--- src/main/java/io/yadnyesh/springcloud/model/Person.java
+++ src/main/java/io/yadnyesh/springcloud/model/Person.java
@@ -1,8 +1,11 @@
 package io.yadnyesh.springcloud.model;
 
+import io.yadnyesh.springcloud.constant.Gender;
+
 public class Person {
 	private Long id;
 	private String firstName;
 	private String lastName;
 	private int age;
+	private Gender gender;
 }
