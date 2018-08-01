e75cc72f621a4878b5fc8045ca7eb35b594c4fdd
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-08-01 19:45:03 +0530

    feat: Added getters and setters

--- src/main/java/io/yadnyesh/springcloud/model/Person.java
+++ src/main/java/io/yadnyesh/springcloud/model/Person.java
@@ -19,4 +19,44 @@ public class Person {
 		this.age = age;
 		this.gender = gender;
 	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public Gender getGender() {
+		return gender;
+	}
+	
+	public void setGender(Gender gender) {
+		this.gender = gender;
+	}
 }
