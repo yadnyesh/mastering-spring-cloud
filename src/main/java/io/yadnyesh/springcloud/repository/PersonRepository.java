9bbe1653fcd1ff61ae4717a77df3bb7e0cf0cef7
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-08-03 11:03:53 +0530

    feat: Method findByAgeGreaterThan

--- src/main/java/io/yadnyesh/springcloud/repository/PersonRepository.java
+++ src/main/java/io/yadnyesh/springcloud/repository/PersonRepository.java
@@ -7,4 +7,5 @@ import java.util.List;
 
 public interface PersonRepository extends MongoRepository<Person, String> {
 	public List<Person> findByLastName(String lastName);
+	public List<Person> findByAgeGreaterThan(int age);
 }
