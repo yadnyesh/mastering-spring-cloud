dd647a100d4b6a9e4ec0b13ab5a4bd46d5736481
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-08-03 11:03:24 +0530

    feat: Method find by Last name

--- src/main/java/io/yadnyesh/springcloud/repository/PersonRepository.java
+++ src/main/java/io/yadnyesh/springcloud/repository/PersonRepository.java
@@ -3,6 +3,8 @@ package io.yadnyesh.springcloud.repository;
 import io.yadnyesh.springcloud.model.Person;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface PersonRepository extends MongoRepository<Person, String> {
+import java.util.List;
 
+public interface PersonRepository extends MongoRepository<Person, String> {
+	public List<Person> findByLastName(String lastName);
 }
